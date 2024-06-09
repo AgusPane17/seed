41b2d5cef1eb199fb124d233c82a11894644167b
Your Name <paulaferreyra24@gmail.com>
2024-06-08 21:23:25 -0300

    Test paula

--- src/main.py
+++ src/main.py
@@ -35,18 +35,35 @@ class TaskManager:
             if hasattr(task, 'due_date'):
                 task_info += f", Due date: {task.due_date}"
                 print(task_info)
-
+    
+    #Metodo para definir las tareas vencidas
+    def get_overdue_tasks(self):
+        duetask = [task for task in self.tasks if task.is_overdue()]
+        print(duetask)
+        return duetask
+    
+   #Metodo para calcular cuanto le falta a una tarea por vencer
+    def time_left_before_due(self):
+        if self.tasks is []:
+            return "No tasks in the task manager"
+        
+        days_left = []
+        for task in self.tasks:  
+            tasks_days_left = f"Task: {task.nameTask}, Days before due date: {task.time_left_before_due()}"
+            days_left.append(tasks_days_left)
+        return days_left
+    
 #Constructor de la clase task, se llama cuando se crea una instancia de task 
 #inicializa todos los atributos de la tarea
 class Task:
-    def __init__(self, nameTag, description=None, priority=None, due_date=None):
-        self.nameTag = nameTag
+    def __init__(self, nameTask, description=None, priority=None, due_date=None):
+        self.nameTask = nameTask
         self.description = description
         self.priority = priority
         if self.priority is not None and priority not in ['A', 'B', 'C']:
             raise ValueError ("La prioridad debe ser: A, B o C")
         if due_date is not None:  # Verificar si due_date se proporcionó
-            self.due_date = datetime.strptime(due_date, "%Y-%m-%d")
+            self.due_date = datetime.strptime(due_date, "%Y-%m-%d").date()
         else:
             self.due_date = None
         self.completed = False
@@ -54,30 +71,27 @@ class Task:
 
     def complete(self):
         self.completed = True
+    
+    #Define si la tarea esta vencida
+    def is_overdue(self):
+        if self.due_date and datetime.now().date() > self.due_date:
+            return True
+        return False
+    
+    #funcion para mostrar el objeto Task en el test
+    def __repr__(self):
+        return f"Task(nameTask= {self.nameTask}, description= {self.description}, priority= {self.priority}, due_date= {self.due_date}, completed= {self.completed})"
+
+    #Metodo para calcular cuanto le falta por vencer a una tarea
+    def time_left_before_due(self):
+        if self.due_date is None:
+            return "No due date set"
+        now = datetime.now().date()
+        time_left = self.due_date - now
+        #Busco los dias de diferencia
+
+        return f"{time_left.days}"
 
 
-#Crear instancia de TaskManager
-task_manager = TaskManager()
-
-# Crear instancias de Task
-task1 = Task(1,"Aprender Python", "A", "2024-06-10")
-
-task2 = Task(2, "Aprender Java", "B", "2024-06-08")
-
-# Añadir la tarea al TaskManager
-task_manager.add_task(task1)
-
-task_manager.add_task(task2)
-
-# Completar la tarea
-task_manager.complete_task(task1)
-
-#Marcar la tarea como completa
-task1.complete()
-
-task_manager.remove_task(task1)
-
-#Traer la lista de tareas 
-task_manager.get_tasks()
 
 
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,14 +1,16 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-from src.main import Task
+from src.main import Task, TaskManager
 import pytest
+from datetime import datetime, timedelta
+
 
 #PANE
 #Test tarea creada
 def test_create_task():
     myTask = Task('Task1')
-    print(myTask.nameTag)
+    print(myTask.nameTask)
 
 if __name__ == "__main__":
     test_create_task()
@@ -16,18 +18,59 @@ if __name__ == "__main__":
 
 #PAULA
 #Test tarea completada
-#pytest para probrar la funcion con parametros
-@pytest.mark.parametrize(
-        
-)
 def test_complete_task():
     myTask = Task('Completed')
     myTask.complete()
     assert myTask.completed == True
     print(myTask.completed)
 
+#Test tareas vencidas
+def test_get_overdue_tasks():
+    task_manager = TaskManager()
+
+    #Obtener las fechas para calcular cuando esta vencida o no
+    now = datetime.now()
+    past_date = (now - timedelta(days=1)).strftime("%Y-%m-%d")
+    future_date = (now + timedelta(days=1)).strftime("%Y-%m-%d")
+
+    #Creo instancias de task, una vencida y otra no
+    overdue_task = Task(1, "Aprender Python", "A", past_date)
+    not_overdue_task = Task(2, "Aprender Java", "B", future_date)
+
+    #Agrego las tareas al administrador
+    task_manager.add_task(overdue_task)
+    task_manager.add_task(not_overdue_task)
+
+    #Obtengo las tareas vencidas
+    overdue_tasks = task_manager.get_overdue_tasks()
+
+    #verifica que esten en la lista correcta(vencida o no)
+    assert overdue_task in overdue_tasks
+    assert not_overdue_task not in overdue_tasks
+
+    print("Overdue tasks:")
+    for task in overdue_tasks:
+        print(task)
+
+if __name__ == "__main__":
+    test_get_overdue_tasks()
+
+#Test de calcular cuanto le falta por vencer a las tareas de la lista
+@pytest.mark.parametrize(
+    "a, b, c, d, e, f, g, h, result",
+    [
+        (1, "Aprender Java", "B", "2024-06-15", 2, "Aprender JS", "B", "2024-06-09", ['Task: 1, Days before due date: 7', 'Task: 2, Days before due date: 1'])
+    ]
+)
+def test_time_left_before_due(a, b, c, d, e, f, g, h, result):
+    task1 = Task(a, b, c, d)
+    task2 = Task(e, f, g, h)
+    task_manager = TaskManager()
+    task_manager.add_task(task1)
+    task_manager.add_task(task2)
+
+    assert task_manager.time_left_before_due() == result
 
 
-# if __name__ == "__main__": 
-#     test_complete_task()
 
+    
